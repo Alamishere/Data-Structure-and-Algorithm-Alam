aa656f86e044c9bb5e0cf5c2e60cec9bd8758d32
Alam Mahmood <alammahmood03@gmail.com>
2024-06-03 11:25:07 -0500

    Add files via upload

---
+++ LinkedlIst/RunLinkedList.java
@@ -0,0 +1,13 @@
+public class RunLinkedliSt {
+    public static void main(String [] args){
+        LinkedList list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.print();
+        list.reverse();
+        list.print();
+    }
+
+}
